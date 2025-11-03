3687a729e479b1e7de11081fbdb9e05ed17cfe95
Suhani <suhaniss885@gmail.com>
2025-11-03 18:47:17 +0530

    feat : active, completed and delete apis

--- models/todo.js
+++ models/todo.js
@@ -4,6 +4,7 @@ const todoSchema = new mongoose.Schema(
   {
     task: { type: String, required: true },
     completed: { type: Boolean, default: false },
+    deleted: { type: Boolean, default: false },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true }
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -11,7 +11,48 @@ function isAuth(req, res, next) {
 
 router.get("/", isAuth, async (req, res, next) => {
   try {
-    const todos = await Todo.find({ user: req.session.userId });
+    const todos = await Todo.find({
+      user: req.session.userId,
+      deleted: { $ne: true },
+    });
+    res.json({ todos });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/active", isAuth, async (req, res, next) => {
+  try {
+    const todos = await Todo.find({
+      user: req.session.userId,
+      completed: false,
+      deleted: { $ne: true },
+    });
+    res.json({ todos });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/completed", isAuth, async (req, res, next) => {
+  try {
+    const todos = await Todo.find({
+      user: req.session.userId,
+      completed: true,
+      deleted: { $ne: true },
+    });
+    res.json({ todos });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/deleted", isAuth, async (req, res, next) => {
+  try {
+    const todos = await Todo.find({
+      user: req.session.userId,
+      deleted: true,
+    });
     res.json({ todos });
   } catch (err) {
     next(err);
@@ -23,7 +64,12 @@ router.post("/", isAuth, async (req, res, next) => {
     const { task } = req.body;
     if (!task) return res.status(400).json({ error: "Task is required" });
 
-    const todo = new Todo({ task, user: req.session.userId, completed: false });
+    const todo = new Todo({
+      task,
+      user: req.session.userId,
+      completed: false,
+      deleted: false,
+    });
     await todo.save();
     res.json({ message: "Todo added", todo });
   } catch (err) {
@@ -35,7 +81,7 @@ router.put("/:id", isAuth, async (req, res, next) => {
   try {
     const { task, completed } = req.body;
     const todo = await Todo.findOneAndUpdate(
-      { _id: req.params.id, user: req.session.userId },
+      { _id: req.params.id, user: req.session.userId, deleted: { $ne: true } },
       { task, completed },
       { new: true }
     );
@@ -48,12 +94,41 @@ router.put("/:id", isAuth, async (req, res, next) => {
 
 router.delete("/:id", isAuth, async (req, res, next) => {
   try {
+    const todo = await Todo.findOneAndUpdate(
+      { _id: req.params.id, user: req.session.userId },
+      { deleted: true },
+      { new: true }
+    );
+    if (!todo) return res.status(404).json({ error: "Todo not found" });
+    res.json({ message: "Todo deleted", todo });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/:id/restore", isAuth, async (req, res, next) => {
+  try {
+    const todo = await Todo.findOneAndUpdate(
+      { _id: req.params.id, user: req.session.userId, deleted: true },
+      { deleted: false },
+      { new: true }
+    );
+    if (!todo) return res.status(404).json({ error: "Deleted todo not found" });
+    res.json({ message: "Todo restored", todo });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id/permanent", isAuth, async (req, res, next) => {
+  try {
     const todo = await Todo.findOneAndDelete({
       _id: req.params.id,
       user: req.session.userId,
+      deleted: true,
     });
-    if (!todo) return res.status(404).json({ error: "Todo not found" });
-    res.json({ message: "Todo deleted" });
+    if (!todo) return res.status(404).json({ error: "Deleted todo not found" });
+    res.json({ message: "Todo permanently deleted" });
   } catch (err) {
     next(err);
   }
