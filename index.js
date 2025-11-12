9776c19562448b06e422993c9fbafc33ff06cba4
Suhani <suhaniss885@gmail.com>
2025-11-12 11:17:26 +0530

    live frontend url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["https://do-bee-do.vercel.app/"],
     credentials: true,
   })
 );
